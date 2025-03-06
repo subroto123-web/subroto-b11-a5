ff7a716a559253d90cc63fda84ad2a8e1d577e22
subroto123 <subrodmagency@gmail.com>
2025-03-06 22:39:07 +0600

    added new js file color-wheel.js and it's code.

---
+++ JavaScript/color-wheel.js
@@ -0,0 +1,11 @@
+function randomColor(){
+    let letters= '0123456789ABCDEF';
+    let color = '#';
+    for(let i = 0; i < 6; i++){
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+document.getElementById('color-change-btn').addEventListener('click', function(){
+    document.getElementById('body').style.background = randomColor();
+});
